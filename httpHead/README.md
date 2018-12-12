2384619ab192b5eb24aa69be74d0f4cae6ae74c8
opera443399 <pc@pcswo.com>
2018-12-12 15:23:35 +0800

    chore(dockerfile): ENV style changed

--- httpHead/README.md
+++ httpHead/README.md
@@ -4,6 +4,6 @@
 
 ## test
 
-docker run --rm opera443399/httphead:0.1.2 -c 3 https://www.qq.com https://www.222qq.com
+docker run --rm opera443399/httphead:0.1.3 -c 3 https://www.qq.com https://www.222qq.com
 
-docker run --rm opera443399/httphead:0.1.2 -c 3 -s https://www.qq.com https://www.222qq.com
+docker run --rm opera443399/httphead:0.1.3 -c 3 -s https://www.qq.com https://www.222qq.com
--- httpHead/build.sh
+++ httpHead/build.sh
@@ -3,9 +3,10 @@
 
 bin_name='httpHead'
 image_name='opera443399/httphead'
-image_ver=$(grep 'ENV APP_VERSION=' Dockerfile |grep -Eo '[0-9].[0-9].[0-9]')
+image_ver=$(grep 'ENV APP_VERSION' Dockerfile |awk '{print $NF}')
 GOARCH="amd64" GOOS="linux" CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o ${bin_name}
 docker build -t ${image_name}:${image_ver} .
+docker tag ${image_name}:${image_ver} ${image_name}
 docker images |grep "${image_name}"
 rm -f ${bin_name}
 
