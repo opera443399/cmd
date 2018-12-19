d9d0c23fd522fadb2bdfc1236393b94a92fd2740
opera443399 <pc@pcswo.com>
2018-12-19 17:53:27 +0800

    fix(docker-API): update to v18.03

--- demoDockerAPI/app.go
+++ demoDockerAPI/app.go
@@ -1,221 +1,164 @@
 /*
  * docker api and sdk exp
- * api ref: https://docs.docker.com/engine/api/v1.3
- * sdk go: https://godoc.org/github.com/moby/moby/client
- 
- * [howto] 
- * # curl -s --unix-socket /var/run/docker.sock http:/v1.30/nodes |jq . |more
- * # curl -s --unix-socket /var/run/docker.sock http:/v1.30/services |jq . |more
- * # curl -s --unix-socket /var/run/docker.sock http:/v1.30/tasks |jq . |more
+ * api ref: https://docs.docker.com/engine/api/v1.37/
+ * sdk go: https://godoc.org/github.com/docker/docker/client
  *
- * pc@2017/8/29
-*/
+ * Server:
+ *  Engine:
+ *   Version:      18.03.1-ce
+ *   API version:  1.37 (minimum version 1.12)
+ *
+ * [howto]
+ * # curl -s --unix-socket /var/run/docker.sock http:/v1.37/services |jq . |more
+ *
+ * pc@2018/12/19
+ */
 
 package main
 
 import (
-    "flag"
-    
-    "strings"
-    "fmt"
-    "log"
-    "encoding/json"
-    
-    "net/http"
-    
-    "context"
-    "github.com/docker/docker/api/types"
-    "github.com/docker/docker/api/types/filters"
-    "github.com/docker/docker/client"
+	"flag"
+	"os"
+
+	"encoding/json"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+
+	"net/http"
+
+	"context"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/filters"
+	"github.com/docker/docker/client"
 )
 
 var port string
 
 func init() {
-    flag.StringVar(&port, "port", "8007", "listen to the given port.")
+	flag.StringVar(&port, "port", "8007", "listen to the given port.")
+	os.Setenv("APP_RUN_ENV", "dev")
+	os.Setenv("DOCKER_API_VERSION", "1.37")
 }
 
 func main() {
-    flag.Parse()
-    // url: index
-    http.HandleFunc("/", index)
-    // url: others
-    http.HandleFunc("/api", api)
+	flag.Parse()
+	http.HandleFunc("/", index)
+	http.HandleFunc("/svc", handlerServices)
 
-    fmt.Println("Listening on port *:" + port)
-    log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Println("Listening on port *:" + port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
-
 
 /*
  * curl 127.0.0.1/index
  *
-*/
+ */
+
+//Project A Project
+type Project struct {
+	Name   string `json:"name"`
+	Status string `json:"status"`
+}
+
+//Projects A list of Projects
+type Projects struct {
+	Env  string    `json:"env"`
+	Data []Project `json:"data"`
+}
 
 func index(w http.ResponseWriter, r *http.Request) {
-    cli, err := client.NewEnvClient()
-    if err != nil {
-        panic(err)
-    }
-    
-    //------ list services
-    fmt.Fprint(w, "├── services:\n")
-    
-    services, err := cli.ServiceList(context.Background(), types.ServiceListOptions{})
-    if err != nil {
-        panic(err)
-    }
+	projectsData := []Project{
+		Project{
+			"demoproject",
+			"1",
+		},
+		Project{
+			"demo1",
+			"0",
+		},
+	}
 
-    for _, service := range services {
-        //------ get Ports
-        this_ports := "" 
-        if len(service.Spec.EndpointSpec.Ports) > 0 {
-            pp := []string{}
-            for _, pConfig := range service.Spec.EndpointSpec.Ports {
-                pp = append(pp, fmt.Sprintf("*:%d->%d/%s",
-                    pConfig.PublishedPort,
-                    pConfig.TargetPort,
-                    pConfig.Protocol,
-                ))
-            }
-            this_ports = strings.Join(pp, ",")
-        }
-        
-        fmt.Fprintf(w, "│   ├── %s \t %s \t %s\n", service.Spec.Name, this_ports, strings.Split(service.Spec.TaskTemplate.ContainerSpec.Image, "@")[0])
-        
-        
-        //------ list nodes which running specific tasks
-        f1 := filters.NewArgs()
-        f1.Add("service", service.ID)
-        f1.Add("desired-state", "running")
-        tasks, err := cli.TaskList(context.Background(), types.TaskListOptions{
-                                                                    Filters: f1,
-                                                                })
-        if err != nil {
-            panic(err)
-        }
-        
-        this_node := make(map[string]string)
-        for _, task := range tasks {        
-            //get node info
-            
-            f2 := filters.NewArgs()
-            f2.Add("id", task.NodeID)
-            nodes, err := cli.NodeList(context.Background(), types.NodeListOptions{
-                                                                    Filters: f2,
-                                                                })
-            if err != nil {
-                panic(err)
-            }
-            
-            node := nodes[0]
-            this_node[node.Description.Hostname] = node.Status.Addr
-        }
-        
-        for k, v := range this_node {
-            fmt.Fprintf(w, "│   │   ├── %s \t %s\n", k, v)
-        }
-        
-    }
-    
+	projects := Projects{
+		"dev",
+		projectsData,
+	}
+
+	if r, err := json.Marshal(projects); err == nil {
+		log.Println("[query-projects] response:", string(r))
+	}
+	json.NewEncoder(w).Encode(projects)
 }
-
 
 /*
- * curl 127.0.0.1/api
+ * curl 127.0.0.1/svc
  *
-*/
+ */
 
-type tasksList struct {
-    IP string `json:"ip"`
-    Hostname string `json:"hostname"`
+//Service A docker swarm service
+type Service struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Replicas string `json:"replicas"`
+	Image    string `json:"image"`
 }
 
-type servicesList struct {
-    Service string `json:"service"`
-    Ports string `json:"ports"`
-    Image string `json:"image"`
-    Tasks []tasksList `json:"tasks"`
+//Services A list of docker swarm services
+type Services struct {
+	Env         string    `json:"env"`
+	ProjectName string    `json:"projectName"`
+	Data        []Service `json:"data"`
 }
 
-type dataList struct {
-    Services []servicesList `json:"data"`
-}
+func handlerServices(w http.ResponseWriter, r *http.Request) {
+	var svcs Services
 
-func api(w http.ResponseWriter, r *http.Request) {
-    var dd dataList
-        
-    cli, err := client.NewEnvClient()
-    if err != nil {
-        panic(err)
-    }
-    
-    //------ list services
-    services, err := cli.ServiceList(context.Background(), types.ServiceListOptions{})
-    if err != nil {
-        panic(err)
-    }
+	env := os.Getenv("APP_RUN_ENV")
+	projectName := "demoproject"
 
-    for _, service := range services {
-        var ss servicesList
-        var tt tasksList
-        
-        ss.Service = service.Spec.Name
-        
-        //------get Ports
-        this_ports := ""
-        if len(service.Spec.EndpointSpec.Ports) > 0 {
-            pp := []string{}
-            for _, pConfig := range service.Spec.EndpointSpec.Ports {
-                pp = append(pp, fmt.Sprintf("*:%d->%d/%s",
-                    pConfig.PublishedPort,
-                    pConfig.TargetPort,
-                    pConfig.Protocol,
-                ))
-            }
-            this_ports = strings.Join(pp, ",")
-        }
-        
-        ss.Ports = this_ports
-        ss.Image = strings.Split(service.Spec.TaskTemplate.ContainerSpec.Image, "@")[0]
-            
-        //------list nodes which running specific tasks
-        f1 := filters.NewArgs()
-        f1.Add("service", service.ID)
-        f1.Add("desired-state", "running")
-        tasks, err := cli.TaskList(context.Background(), types.TaskListOptions{
-                                                                    Filters: f1,
-                                                                })
-        if err != nil {
-            panic(err)
-        }
-        
-        this_node := make(map[string]string)
-        for _, task := range tasks {          
-            //node
-            
-            f2 := filters.NewArgs()
-            f2.Add("id", task.NodeID)
-            nodes, err := cli.NodeList(context.Background(), types.NodeListOptions{
-                                                                    Filters: f2,
-                                                                })
-            if err != nil {
-                panic(err)
-            }
-            
-            node := nodes[0]
-            this_node[node.Description.Hostname] = node.Status.Addr
-        }
-        
-        for k, v := range this_node {
-            tt.Hostname = k
-            tt.IP = v
-            ss.Tasks = append(ss.Tasks, tt)
-        }
-        
-        dd.Services = append(dd.Services, ss)
-    }
-    
-    json.NewEncoder(w).Encode(dd)
-    
+	servicePrefix := env + "-" + projectName
+	log.Println("[filter-service] name =", servicePrefix)
+
+	svcs.Env = env
+	svcs.ProjectName = projectName
+
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		panic(err)
+	}
+
+	//------ list service with filter
+	fSvc := filters.NewArgs()
+	fSvc.Add("name", servicePrefix)
+	services, err := cli.ServiceList(context.Background(), types.ServiceListOptions{Filters: fSvc})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, s := range services {
+		svc := Service{}
+
+		//------ list task with filter
+		fTask := filters.NewArgs()
+		fTask.Add("service", s.ID)
+		fTask.Add("desired-state", "running")
+		tasks, err := cli.TaskList(context.Background(), types.TaskListOptions{Filters: fTask})
+		if err != nil {
+			panic(err)
+		}
+
+		svc.ID = s.ID[:10]
+		svc.Name = s.Spec.Name
+		svc.Replicas = fmt.Sprintf("%d/%s", len(tasks), strconv.FormatUint(*s.Spec.Mode.Replicated.Replicas, 10))
+		image := strings.Split(strings.Split(s.Spec.TaskTemplate.ContainerSpec.Image, "@")[0], "/")
+		svc.Image = image[len(image)-1]
+		svcs.Data = append(svcs.Data, svc)
+	}
+
+	if r, err := json.Marshal(svcs); err == nil {
+		log.Println("[query-services] response:", string(r))
+	}
+	json.NewEncoder(w).Encode(svcs)
 }
